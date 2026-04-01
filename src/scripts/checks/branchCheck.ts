8ce7766989509e1f5bea0ec132d97f2d671fedf2
James <jb.007.db05@gmail.com>
2026-04-01 14:00:43 +0530

    feat: implemented ci for github

--- src/scripts/checks/branchCheck.ts
+++ src/scripts/checks/branchCheck.ts
@@ -1,5 +1,10 @@
 import { execSync } from "child_process";
 
+if (process.env.CI === "true") {
+  console.log("⚠️ Skipping branch check in CI");
+  process.exit(0);
+}
+
 const branch = execSync("git rev-parse --abbrev-ref HEAD").toString().trim(); // get current branch name
 
 const validPattern = /^(feat|fix|hotFix|refactor|test)\/.+$/;
